142c7b67a8370d4e26e1256025fa0bcde2259760
mikhail Panin <adivanced@gmail.com>
2019-01-08 17:50:03 +0300

    Init

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/order' do
+  erb :order
+end
